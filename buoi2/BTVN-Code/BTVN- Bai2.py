357d05c5c864df625a769fa44bc4a927062c7c1e
osgi23 <manhquan03122005@gmail.com>
2024-07-19 23:39:07 +0700

    BTVN-Buoi2-bai2

---
+++ buoi2/BTVN-Code/BTVN- Bai2.py
@@ -0,0 +1,21 @@
+a = int(input())
+b = int(input())
+
+print(a + b)
+print(a - b)
+print(a * b)
+print(a // b)
+print(a ** b)
+print(a % b)
+result = 1
+if a == b:
+    result = 0
+elif a < b:
+    reselt = -1
+print(result)
+print(a & b)
+print(a | b)
+print(a ^ b)
+print(~(a == b))
+print(a >> 1)
+print(a << 1)
